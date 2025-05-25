5c7be0984453003421933a4c697d2d969c8e650f
Jayki79 <j.kirsch@schule.saarland>
2025-05-25 10:56:05 +0200

    Grundlagen implementiert

---
+++ script.js
@@ -0,0 +1,10 @@
+const loginButton = document.querySelector('.login-button');
+
+loginButton.addEventListener('click', function(event) {
+    event.preventDefault(); // Verhindert, dass das Formular abgeschickt wird (falls im Formular)
+    meineFunktion();
+});
+
+function meineFunktion() {
+    window.location.href = 'welcome.html';
+}
